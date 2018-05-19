bea82096d419b94bfae8b2262d1d77eee2fa1684
Hugo Diniz <hhldiniz@gmail.com>
2018-05-19 14:48:38 -0300

    pequena refatoração e criação da base das views html

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
+from views.IndexView import IndexView
+
 app = Flask(__name__)
 # http://mysafeinfo.com/api/data?list=moviesbyearnings2010&format=html&alias=yr=year,rank=rank,tit=tittle,grs=gross,opn=opened,cnt=theatres
 
+index_view = IndexView(template_name="index.html")
+app.add_url_rule("/", view_func=index_view.as_view("index", template_name=index_view.get_template_name()))
 
 if __name__ == '__main__':
     app.run()
--- views/BaseView.py
+++ views/BaseView.py
@@ -5,15 +5,25 @@ from flask.views import View
 class BaseView(View):
     def __init__(self, template_name):
         self.__template_name = template_name
+        self.__title = "Title"
+
+    def set_title(self, title):
+        self.__title = title
+
+    def get_title(self):
+        return self.__title
 
     def dispatch_request(self):
         if request.method == "GET":
-            return self.get()
+            return self.get(title=self.get_title())
         elif request.method == "POST":
-            return self.post()
+            return self.post(title=self.get_title())
 
     def get(self, **context):
         return render_template(self.__template_name, **context)
 
     def post(self, **context):
         return render_template(self.__template_name, **context)
+
+    def get_template_name(self):
+        return self.__template_name
